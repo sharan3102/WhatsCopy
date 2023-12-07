39e8580046d8538592d845f96ecbf6f10a909f6c
sharan3102 <2020it0134@svce.ac.in>
2023-12-07 23:38:51 +0530

    feat(emoji): modify script to copy emoji - Whatsapp renders emojis as images - Identify the element whether it is IMG - Copy the 'alt' text of the image

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("This is content script")
-
+// Function to copy text to clipboard
 function copyToClipboard(text) {
     const textarea = document.createElement('textarea');
     textarea.value = text;
@@ -13,78 +13,55 @@ function copyToClipboard(text) {
     document.body.removeChild(textarea);
 }
 
+// Function to add the copy icon to messages
 function addCopyIconToMessages(){
-
-    // get messages
+    // Find all message containers
     const messages = document.querySelectorAll('.message-in');
     console.log(messages)
 
+    // Iterate through each message
     messages.forEach(message => {
-
-        // get container
         const iconContainer = message.querySelector('.tvf2evcx.m0h2a7mj.lb5m6g5c.j7l1k36l.ktfrpxia.nu7pwgvd.p357zi0d.dnb887gk.gjuq5ydh.i2cterl7.FxqSn');
-        
-        // create new button
         const copyButton = document.createElement('button');
         copyButton.classList.add('copy-button');
+        copyButton.style.background= '#040608f'
 
-        // add same class of reaction button
+        copyButton.style.width=32
+        copyButton.style.width=32
         const classes = 'tvf2evcx m0h2a7mj lb5m6g5c j7l1k36l ktfrpxia nu7pwgvd dnb887gk gjuq5ydh i2cterl7 rqm6ogl5 i5tg98hk folpon7g przvwfww snweb893'.split(' ')
         classes.map(className => {
             copyButton.classList.add(className)
         })
-
-        // adding icon
         copyButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path d="M7 3.5A1.5 1.5 0 018.5 2h3.879a1.5 1.5 0 011.06.44l3.122 3.12A1.5 1.5 0 0117 6.622V12.5a1.5 1.5 0 01-1.5 1.5h-1v-3.379a3 3 0 00-.879-2.121L10.5 5.379A3 3 0 008.379 4.5H7v-1z" /><path d="M4.5 6A1.5 1.5 0 003 7.5v9A1.5 1.5 0 004.5 18h7a1.5 1.5 0 001.5-1.5v-5.879a1.5 1.5 0 00-.44-1.06L9.44 6.439A1.5 1.5 0 008.378 6H4.5z" /></svg>'
 
-        // add button to icon container
+        // Append your copy icon button to the existing icon container
         iconContainer.appendChild(copyButton);
         message.addEventListener('mouseover', () => {
             copyButton.style.display = 'block';
         });
 
-        // toggle button
         message.addEventListener('mouseout', () => {
             copyButton.style.display = 'none';
         });
-
-        // add event listener to the copy button
+        // Add click event listener to the copy button
         copyButton.addEventListener('click', () => {
-
-            // get message text
+            // Get the message text
             const copyableText = message.querySelector('.copyable-text span');
-            let messageText = ''
-            // copyableText ? copyableText.textContent.trim() : '';
-            if(copyableText){
-                console.log(copyableText)
-                console.log(copyableText.children[0])
+            const messageText = copyableText ? copyableText.textContent.trim() : '';
 
-                if(copyableText.children[0].tagName === 'IMG' ){
-                    console.log("Inside IFFFF ---------------->")
-                    messageText = copyableText.children[0].alt;
-                    console.log(messageText)
-                }
-                else{
-                    console.log("Inside ELSE ---------------->")
-                    messageText = copyableText.textContent.trim();
-                    console.log(messageText)
-                }
-            }
-
-            // copy the message text to clipboard
+            // Copy the message text to clipboard
             if (messageText) {
                 copyToClipboard(messageText);
 
-                // set user feedback
                 copyButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>';
 
-                // revert the icon to normal one
                 setTimeout(() => {
                     copyButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path d="M7 3.5A1.5 1.5 0 018.5 2h3.879a1.5 1.5 0 011.06.44l3.122 3.12A1.5 1.5 0 0117 6.622V12.5a1.5 1.5 0 01-1.5 1.5h-1v-3.379a3 3 0 00-.879-2.121L10.5 5.379A3 3 0 008.379 4.5H7v-1z" /><path d="M4.5 6A1.5 1.5 0 003 7.5v9A1.5 1.5 0 004.5 18h7a1.5 1.5 0 001.5-1.5v-5.879a1.5 1.5 0 00-.44-1.06L9.44 6.439A1.5 1.5 0 008.378 6H4.5z" /></svg>';
                 }, 1500);
             }
-
         });
     });
 
 }
+// Call the function when the DOM content is loaded
+// document.addEventListener('DOMContentLoaded', addCopyIconToMessages());
